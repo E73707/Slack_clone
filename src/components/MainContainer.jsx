945963f1a311639a2b4c291e36dfebfc5a326f2f
E73707 <94885681+E73707@users.noreply.github.com>
2024-08-26 12:03:20 +1000

    added resizable element functionality with react-resizable-panels

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,30 +1,59 @@
-import react from "react";
-import { getAuth, signOut } from "firebase/auth";
+// import React from "react";
+// import { ResizableBox } from "react-resizable";
+// import "../css/MainContainer.css";
+// import LeftMainContainer from "./LeftMainContainer";
+// import RightMainContainer from "./RightMainContainer";
+// import "react-resizable/css/styles.css";
+
+// export default function MainContainer() {
+//   return (
+//     <div className="main-container-2">
+//       <ResizableBox
+//         className="main-container-2-left-wrapper"
+//         width={300}
+//         height={Infinity}
+//         minConstraints={[100, Infinity]}
+//         maxConstraints={[600, Infinity]}
+//         axis="x"
+//       >
+//         <LeftMainContainer />
+//       </ResizableBox>
+//       <ResizableBox
+//         className="main-container-2-right-wrapper"
+//         width={300}
+//         height={Infinity}
+//         minConstraints={[100, Infinity]}
+//         maxConstraints={[600, Infinity]}
+//         axis="x"
+//       >
+//         <RightMainContainer />
+//       </ResizableBox>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import "../css/MainContainer.css";
 import LeftMainContainer from "./LeftMainContainer";
 import RightMainContainer from "./RightMainContainer";
 
 export default function MainContainer() {
-  //   const auth = getAuth();
-  //   async function handleSignout() {
-  //     try {
-  //       await signOut(auth);
-  //       console.log("User signed out successfully");
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-
   return (
     <div className="main-container-2">
-      <div className="main-container-2-left-wrapper">
-        <LeftMainContainer />
-        {/* <h1>This is the MainContainer component</h1>
-        <button onClick={() => handleSignout()}>Sign out</button> */}
-      </div>
-      <div className="main-container-2-right-wrapper">
-        <RightMainContainer />
-      </div>
+      <PanelGroup direction="horizontal">
+        <Panel className="left-panel" defaultSize={25} minSize={10}>
+          <div className="main-container-2-left-wrapper">
+            <LeftMainContainer />
+          </div>
+        </Panel>
+        <PanelResizeHandle className="resize-handle" />
+        <Panel defaultSize={75} minSize={20}>
+          <div className="main-container-2-right-wrapper">
+            <RightMainContainer />
+          </div>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
