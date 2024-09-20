40aa748085f19ae32255408648bd918c970d203e
E73707 <94885681+E73707@users.noreply.github.com>
2024-09-20 15:25:01 +1000

    debugging

--- server/models/index.js
+++ server/models/index.js
@@ -8,71 +8,55 @@ import ChannelMember from "./ChannelMember.js";
 import InviteToken from "./InviteToken.js";
 
 // Associations
-
-// Many-to-Many relationship between Users and Communities through CommunityMember
 User.belongsToMany(Community, {
   through: CommunityMember,
-  as: "memberOf", // Alias for communities the user is a member of
+  as: "memberOf",
 });
 Community.belongsToMany(User, {
   through: CommunityMember,
-  as: "members", // Alias for users who are members of a community
+  as: "members",
 });
-// A Community has one owner (a User)
 Community.belongsTo(User, {
-  as: "owner", // Alias for the owner of the community
+  as: "owner",
   foreignKey: "community_owner",
 });
-
 User.hasMany(Community, {
-  as: "ownedCommunities", // Alias for communities the user owns
+  as: "ownedCommunities",
   foreignKey: "community_owner",
 });
-
-// One-to-Many relationship between Community and CommunityChannel
 Community.hasMany(CommunityChannel, {
   foreignKey: "communityId",
   as: "channels",
 });
 CommunityChannel.belongsTo(Community, { foreignKey: "communityId" });
-
-// One-to-Many relationship between Community and CommunityPost
 Community.hasMany(CommunityPost, { foreignKey: "communityId" });
 CommunityPost.belongsTo(Community, { foreignKey: "communityId" });
-
-// One-to-Many relationship between User and CommunityPost
-
 User.hasMany(CommunityPost, { foreignKey: "userId" });
 CommunityPost.belongsTo(User, { foreignKey: "userId" });
-
-// One-to-Many relationship between CommunityChannel and CommunityPost
 CommunityChannel.hasMany(CommunityPost, { foreignKey: "channelId" });
 CommunityPost.belongsTo(CommunityChannel, { foreignKey: "channelId" });
-
 User.belongsToMany(CommunityChannel, {
   through: ChannelMember,
   as: "channel_members",
 });
-
 CommunityChannel.hasMany(ChannelMember, {
   foreignKey: "channelId",
   as: "channel_members",
 });
-
 ChannelMember.belongsTo(CommunityChannel, {
   foreignKey: "channelId",
   as: "channel_members",
 });
 
 // Sync all models with the database
-sequelize
-  .sync({ force: false })
-  .then(() => {
+(async () => {
+  try {
+    await sequelize.sync({ force: false });
     console.log("Database synced and tables created");
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Error syncing database:", error);
-  });
+  }
+})();
 
 export {
   User,
